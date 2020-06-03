58be30f53bab94c59ca182d3dbbe1b8e69e2cc42
tussath <ussath@youbility.de>
2020-06-03 12:26:34 +0200

    clean metadata.php

--- metadata.php
+++ metadata.php
@@ -51,8 +51,6 @@ $aModule = array(
     'extend' => array(
         \OxidEsales\Eshop\Application\Model\Search::class => \Semknox\Productsearch\Application\Model\Search::class,
         \OxidEsales\Eshop\Application\Controller\SearchController::class => \Semknox\Productsearch\Application\Controller\SearchController::class
-        /*\OxidEsales\Eshop\Application\Model\ArticleList::class => \Semknox\Productsearch\Application\Model\ArticleList::class,*/
-        /*\OxidEsales\Eshop\Application\Controller\FrontendController:: => \Semknox\Productsearch\Application\Controller\FrontendController::class*/
     ),
     'templates' => array(
         'admin_sxproductsearch_ajax.tpl'   => 'semknox/semknox-oxid/Application/views/admin/tpl/admin_sxproductsearch_ajax.tpl',
@@ -64,12 +62,7 @@ $aModule = array(
             'block'=>'admin_bottomnavicustom',
             'file'=>'Application/views/admin/blocks/admin_bottomnavicustom.tpl'
         ),
-    ),
-    /*
-    'events' => array(
-        'onActivate' => null,//'SemknoxProductsearch_init::onActivate',
-        'onDeactivate' => null,//'SemknoxProductsearch_init::onDeactivate'
-    )*/
+    )
 );
 
 $settings = array();
